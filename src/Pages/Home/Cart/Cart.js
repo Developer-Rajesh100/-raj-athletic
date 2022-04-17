143e1ed2a28f021595dc11356e41256dbee60d59
Developer-Rajesh <rajeshhabra100@gmail.com>
2022-04-17 19:18:38 +0530

    Navbar complete

--- src/Pages/Home/Cart/Cart.js
+++ src/Pages/Home/Cart/Cart.js
@@ -1,17 +1,11 @@
 import React from "react";
 import "./Cart.css";
-// import sprint from "../../../Images/Cart Img/sprint.jpeg";
-// import highJump from "../../../Images/Cart Img/high-jump.jpg";
-// import hurdles from "../../../Images/Cart Img/hurdles.png";
-// import javelinThrow from "../../../Images/Cart Img/javelin-throw.jpg";
-// import longJump from "../../../Images/Cart Img/long-jump.jpg";
-// import poalVolt from "../../../Images/Cart Img/poal-volt.jpg";
-const Cart = (props) => {
-  const [key, photo, service, fee] = props;
+const Cart = (Data) => {
+  // const [key, photo, service, fee] = props;
   return (
     <div className="container">
       <div>
-        <img src={photo} alt="" />
+        <h1>{Data.langth}</h1>
       </div>
     </div>
   );
--- src/Pages/Home/NavBar/NavBar.js
+++ src/Pages/Home/NavBar/NavBar.js
@@ -1,9 +1,40 @@
 import React from "react";
-
+import { Container, Nav, Navbar, NavDropdown } from "react-bootstrap";
+import "./NavBar.css";
 const NavBar = () => {
   return (
-    <div className="App">
-      <h1>This is NavBAr....</h1>
+    <div>
+      <Navbar className="navBackground" bg="" expand="lg">
+        <Container>
+          <Navbar.Brand href="#home">Raj Athletic</Navbar.Brand>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav>
+              <div className="d-flex justify-content-around">
+                <Nav.Link href="/">Home</Nav.Link>
+                <Nav.Link href="#link">Blogs</Nav.Link>
+                <Nav.Link href="#link">About Me</Nav.Link>
+                <Nav.Link href="#link">Sign In</Nav.Link>
+                <Nav.Link href="#link">Sign Up</Nav.Link>
+              </div>
+
+              {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown">
+                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.2">
+                  Another action
+                </NavDropdown.Item>
+                <NavDropdown.Item href="#action/3.3">
+                  Something
+                </NavDropdown.Item>
+                <NavDropdown.Divider />
+                <NavDropdown.Item href="#action/3.4">
+                  Separated link
+                </NavDropdown.Item>
+              </NavDropdown> */}
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
     </div>
   );
 };
